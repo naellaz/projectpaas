8873514feaecb79071aac281a79703a0b4c68cf3
naellaz <studynaell@gmail.com>
2025-10-07 13:11:56 +0700

    Fix: duplicate express declaration

--- server.js
+++ server.js
@@ -5,7 +5,6 @@ const multer = require('multer');
 const path = require('path');
 const fs = require('fs');
 
-const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
